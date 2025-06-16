633030117d9f95ca8ce63f5f24e2546b87ed1e7c
prevter <prevter@gmail.com>
2025-06-16 11:37:39 +0300

    fix: migrate old downloads table

--- migrations/20250616061604_blake3.sql
+++ migrations/20250616061604_blake3.sql
@@ -16,5 +16,20 @@ CREATE TABLE files (
 INSERT INTO files (id, name, stored_name, size, type, created_at, hash)
 SELECT id, name, stored_name, size, type, created_at, '' AS hash FROM files_old;
 
--- 4. drop old table
+-- 4. move downloads table to new table
+ALTER TABLE downloads RENAME TO downloads_old;
+
+-- 5. create a new downloads table with file_id as a foreign key
+CREATE TABLE downloads (
+    file_id INTEGER NOT NULL UNIQUE,
+    count INTEGER NOT NULL DEFAULT 0,
+    FOREIGN KEY (file_id) REFERENCES files (id)
+);
+
+-- 6. copy data from old downloads table to new downloads table
+INSERT INTO downloads (file_id, count)
+SELECT file_id, count FROM downloads_old;
+
+-- 7. drop old tables
 DROP TABLE files_old;
+DROP TABLE downloads_old;
